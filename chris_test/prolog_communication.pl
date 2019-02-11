8f3e6494e1f141ebc02b922592e19254be8c42d1
clarkatron <chrisjclark@gmail.com>
2019-02-10 19:32:10 -0800

    adding in my prolog files

--- chris_test/prolog_communication.pl
+++ chris_test/prolog_communication.pl
@@ -1,11 +1,35 @@
 :- initialization readfile.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Defining the game board          %%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+neimat(10, 11, 21).
+neimatl(11, 10, 12, 00).
+neimat(12, 11, 23).
+neimat(20, 23, 31).
+neimatl(21, 20, 10, 22).
+neimatl(22, 23, 33, 21).
+neimatl(23, 12, 22, 24).
+neimat(24, 35, 23).
+neimatl(31, 30, 20, 32).
+neimat(32, 31, 33).
+neimatl(33, 32, 34, 22).
+neimat(34, 33, 35).
+neimatl(35, 34, 24, 36).
+
+% are_neighbors(X, _, _,_) :-
+
 readfile :-
     open('myFile.txt', read, str),
-    open('commandsFile.txt', append, cmds), 
+    % open('myFile.txt', append, neighs), 
     read_string(str, "\t", "\r", End, Lines),
+    % find_neighbors(Lines),
     close(str),
-    close(cmds
+    % close(neighs),
     write(Lines), 
     halt.
 
+% find_neighbors(loc) :-
+	
