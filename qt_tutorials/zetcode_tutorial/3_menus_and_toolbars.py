ea1ac2cdeb717c5caf704435ec504d2b0c9305aa
charlesstoll <charlesrstoll@gmail.com>
2019-02-12 12:10:30 -0800

    More zetcode tutorial stuff
    adding more zetcode qt tutorials

---
+++ qt_tutorials/zetcode_tutorial/3_menus_and_toolbars.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+class Example(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        exitAct = QAction(QIcon('mario.png'), '&Exit', self)
+        exitAct.setShortcut('Ctrl+Q')
+        exitAct.setStatusTip('Exit application')
+        exitAct.triggered.connect(qApp.quit)
+
+        self.statusBar().showMessage('Ready')
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Statusbar')
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
