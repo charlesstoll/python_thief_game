4a70ed5700f02d5dcb544163fba8bc13f8740077
emmajingsmith <emma25@pdx.edu>
2019-03-19 22:10:44 +0000

    Should be ready to turn and do happy dance

--- communication/server.py
+++ communication/server.py
@@ -107,10 +107,15 @@ def move_robot(degrees, distance):
             sleep_time = distance * 1 
             RobotFWD(float(sleep_time))
     elif robot_type == 'vikingbot1':
-        turn_to_angle(degrees)
-        # Multiplier tbd
-        sleep_time = distance * 1
-        RobotFWD(float(sleep_time))
+        if degrees == '800':
+            RobotBACK(0.5)
+            RobotFWD(0.5)
+            RobotRIGHT(2.2)
+        else:
+            turn_to_angle(degrees)
+            # Multiplier tbd
+            sleep_time = distance * 1 
+            RobotFWD(float(sleep_time))
 
 def send_motion_command(client_command, motion_command_dict):
     """
